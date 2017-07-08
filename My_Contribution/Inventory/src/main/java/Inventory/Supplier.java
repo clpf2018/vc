e4a27f87ddf8e547c34861c9c59c2905c9d38944
joseph743 <joseph.elia@isae.edu.lb|git config --global user.email joseph.elia@isae.edu.lb>
2017-07-08 18:57:00 +0300

    new file:   My_Contribution/Inventory/pom.xml 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Address.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Barcode.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Entrepot.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Individual.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Localization.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Location.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Location_Type.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Operation.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Person.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Strategy.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Supplier.java 	new file:   My_Contribution/Inventory/src/main/java/Inventory/Vendor.java 	new file:   My_Contribution/Inventory/target/classes/.netbeans_automatic_build 	new file:   My_Contribution/Inventory/target/classes/Inventory/Address.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Barcode.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Entrepot.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Individual.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Localization.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Location.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Location_Type.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Operation.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Person.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Strategy.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Supplier.class 	new file:   My_Contribution/Inventory/target/classes/Inventory/Vendor.class 	new file:   My_Contribution/Inventory/target/test-classes/.netbeans_automatic_build

---
+++ My_Contribution/Inventory/src/main/java/Inventory/Supplier.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inventory;
+
+/**
+ *
+ * @author joseph
+ */
+public class Supplier extends Person {
+    
+    public Supplier(String ID, String Phone, String Mobile, String Fax, String Email) {
+        super(ID, Phone, Mobile, Fax, Email);
+    }
+    
+    
+}
