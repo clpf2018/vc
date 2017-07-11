221ca54d5be4062e2baa0173302bfb616c20110c
ansari007 <mohamadansari1994@gmail.com>
2017-07-11 22:25:02 +0300

    Adding project infrastructure

---
+++ WebServicesApplication/src/java/com/services/ApplicationConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.services;
+
+/**
+ *
+ * @author mohamad
+ */
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("resources")
+public class ApplicationConfig extends Application {
+
+}
